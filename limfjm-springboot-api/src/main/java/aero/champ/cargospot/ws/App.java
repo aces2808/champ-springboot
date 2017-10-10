3469a5cb361d7697a75d7ff4e747b4378a4d6fcd
LIM Francis Joseph <limfr@champ.aero>
2017-10-10 11:49:22 +0800

    mvn eclipse

---
+++ limfjm-springboot-api/src/main/java/aero/champ/cargospot/ws/App.java
@@ -0,0 +1,13 @@
+package aero.champ.cargospot.ws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
